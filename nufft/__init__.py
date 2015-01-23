f17bf7ca242b7f484481d67eaa65cf702f080699
Dan F-M <danfm@nyu.edu>
2015-01-23 15:36:01 -0500

    adding type 2 but not working yet

--- nufft/__init__.py
+++ nufft/__init__.py
@@ -8,5 +8,5 @@ except NameError:
     __NUFFT_SETUP__ = False
 
 if not __NUFFT_SETUP__:
-    __all__ = ["nufft1", "nufft3"]
-    from .nufft import nufft1, nufft3
+    __all__ = ["nufft1freqs", "nufft1", "nufft2", "nufft3"]
+    from .nufft import nufft1freqs, nufft1, nufft2, nufft3
