80e0f3a0db408d837ece3c59702469f9322e29ea
Dan F-M <danfm@nyu.edu>
2015-01-23 15:25:10 -0500

    fixing some f2py memory bugs

--- nufft/nufft.py
+++ nufft/nufft.py
@@ -2,11 +2,12 @@
 
 from __future__ import division, print_function
 
-__all__ = ["nufft1freqs", "nufft1", "nufft3"]
+__all__ = ["nufft1freqs", "nufft1", "nufft2", "nufft3"]
 
 import numpy as np
 from ._nufft import (
     dirft1d1, nufft1d1f90,
+    dirft1d2, nufft1d2f90,
     dirft1d3, nufft1d3f90,
 )
 
@@ -29,8 +30,24 @@ def nufft1(x, y, ms, df=1.0, eps=1e-15, iflag=1, direct=False):
         p, flag = nufft1d1f90(x * df, y, iflag, eps, ms)
         # Check the output and return.
         if flag:
-            raise RuntimeError("nufft1d3 failed with code {0}".format(flag))
+            raise RuntimeError("nufft1d1 failed with code {0}".format(flag))
     return p
+
+
+def nufft2(x, p, df=1.0, eps=1e-15, iflag=1, direct=False):
+    # Make sure that the data are properly formatted.
+    x = np.ascontiguousarray(x, dtype=np.float64)
+    p = np.ascontiguousarray(p, dtype=np.complex128)
+
+    # Run the Fortran code.
+    if direct:
+        y = dirft1d2(x * df, iflag, p)
+    else:
+        y, flag = nufft1d2f90(x * df, iflag, eps, p)
+        # Check the output and return.
+        if flag:
+            raise RuntimeError("nufft1d2 failed with code {0}".format(flag))
+    return y
 
 
 def nufft3(x, y, f, eps=1e-15, iflag=1, direct=False):
