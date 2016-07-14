89a538c2272c73ec8f4ad9a3332843dde21f18e9
Thomas Arildsen <tha@es.aau.dk>
2016-07-14 10:43:22 +0200

    WIP: Attempting to convert tests to `unittest` framework
    So far I am having trouble figuring out how to make the tests runnable.
    The package's relative imports are causing me trouble.

--- nufft/test.py
+++ nufft/test.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import division, print_function
+import unittest
 
 __all__ = ["test_type_1", "test_type_2", "test_type_3", "test_1_and_3"]
 
@@ -45,88 +46,107 @@ def _get_data_roundtrip():
     return x, y, f
 
 
-def _type_1(eps=1e-10):
-    x, y, f = _get_data()
-    p2 = nufft1(x, y, len(f), direct=True)
-    p1 = nufft1(x, y, len(f), eps=eps)
-    err = np.sqrt(np.sum(np.abs(p1 - p2) ** 2) / np.sum(np.abs(p1)**2))
-    assert err < eps
-    assert len(nufft1freqs(len(f))) == len(p1), "even"
+class TestType1(unittest.TestCase):
 
-    p2 = nufft1(x, y, len(f) + 1, direct=True)
-    p1 = nufft1(x, y, len(f) + 1, eps=eps)
-    err = np.sqrt(np.sum(np.abs(p1 - p2) ** 2) / np.sum(np.abs(p1)**2))
-    assert err < eps
-    assert len(nufft1freqs(len(f) + 1)) == len(p1), "odd"
+    def setUp(self):
+        self.x, self.y, self.f = _get_data()
 
+    def _type_1_even(self, eps=1e-10):
+        p2 = nufft1(self.x, self.y, len(self.f), direct=True)
+        p1 = nufft1(self.x, self.y, len(self.f), eps=eps)
+        err = np.sqrt(np.sum(np.abs(p1 - p2) ** 2) / np.sum(np.abs(p1)**2))
+        self.assertTrue(err < eps, "Discrepancy between direct and fft function")
+        self.assertEqual(len(nufft1freqs(len(f))), len(p1), "Wrong length of frequency array")
 
-def test_type_1():
-    for eps in [1e-2, 1e-5, 1e-10, 1e-12]:
-        _type_1(eps)
+    def _type_1_odd(self, eps=1e-10):
+        p2 = nufft1(self.x, self.y, len(self.f) + 1, direct=True)
+        p1 = nufft1(self.x, self.y, len(self.f) + 1, eps=eps)
+        err = np.sqrt(np.sum(np.abs(p1 - p2) ** 2) / np.sum(np.abs(p1)**2))
+        self.assertTrue(err < eps, "Discrepancy between direct and fft function")
+        self.assertEqual(len(nufft1freqs(len(f) + 1)), len(p1), "Wrong length of frequency array")
 
+    def test_type_1_even_2(self):
+        eps = 1e-2
+        err = _type_1_even(self, eps)
+        self.assertTrue(err < eps, "Discrepancy between direct and fft function")
 
-def _type_2(eps=1e-10):
-    x, y, f = _get_data()
-    y2 = nufft2(x, f, direct=True)
-    y1 = nufft2(x, f, eps=eps)
-    err = np.sqrt(np.sum(np.abs(y1 - y2) ** 2) / np.sum(np.abs(y1)**2))
-    assert err < eps
+    def test_type_1_even_5(self):
+        eps = 1e-5
+        err = _type_1_even(self, eps)
+        self.assertTrue(err < eps, "Discrepancy between direct and fft function")
 
+    def test_type_1_even_10(self):
+        eps = 1e-10
+        err = _type_1_even(self, eps)
+        self.assertTrue(err < eps, "Discrepancy between direct and fft function")
 
-def test_type_2():
-    for eps in [1e-2, 1e-5, 1e-10, 1e-12]:
-        _type_2(eps)
+    def test_type_1_even_12(self):
+        eps = 1e-12
+        err = _type_1_even(self, eps)
+        self.assertTrue(err < eps, "Discrepancy between direct and fft function")
 
-
-def _type_1_2_roundtrip(eps=1e-10):
-    x, y1, f = _get_data_roundtrip()
-    p = nufft1(x, y1, len(f), iflag=-1, eps=eps)
-    y2 = len(x) / len(f) * nufft2(x, p, iflag=1, direct=True)
-    err = np.sqrt(np.sum(np.abs(y1 - y2) ** 2) / np.sum(np.abs(y1)**2))
-    assert err < eps
-
-
-def test_type_1_2_roundtrip():
-    for eps in [1e-2, 1e-5, 1e-10, 1e-12]:
-        _type_1_2_roundtrip(eps)
-
-
-def _type_3(eps=1e-10):
-    x, y, f = _get_data()
-    p2 = nufft3(x, y, f, direct=True)
-    p1 = nufft3(x, y, f, eps=eps)
-    err = np.sqrt(np.sum(np.abs(p1 - p2) ** 2) / np.sum(np.abs(p1)**2))
-    assert err < eps
-
-
-def test_type_3():
-    for eps in [1e-2, 1e-5, 1e-10, 1e-12]:
-        _type_3(eps)
-
-
-def _type_1_and_3(eps=1e-10):
-    x, y, f = _get_data()
-
-    f = nufft1freqs(len(f))
-    p2 = nufft3(x, y, f, eps=eps)
-    p1 = nufft1(x, y, len(f), eps=eps)
-    err = np.sqrt(np.sum(np.abs(p1 - p2) ** 2) / np.sum(np.abs(p1)**2))
-    assert err < eps, "even"
-
-    f = nufft1freqs(len(f) + 1)
-    p2 = nufft3(x, y, f, eps=eps)
-    p1 = nufft1(x, y, len(f), eps=eps)
-    err = np.sqrt(np.sum(np.abs(p1 - p2) ** 2) / np.sum(np.abs(p1)**2))
-    assert err < eps, "odd"
-
-    df = 0.5 * (f[1] - f[0])
-    p1 = nufft1(x, y, len(f), eps=eps, df=df)
-    f = nufft1freqs(len(f), df=df)
-    p2 = nufft3(x, y, f, eps=eps)
-    err = np.sqrt(np.sum(np.abs(p1 - p2) ** 2) / np.sum(np.abs(p1)**2))
-    assert err < eps, "even"
-
-
-def test_1_and_3():
-    for eps in [1e-2, 1e-5, 1e-10, 1e-12]:
-        _type_1_and_3(eps)
+#def _type_2(eps=1e-10):
+#    x, y, f = _get_data()
+#    y2 = nufft2(x, f, direct=True)
+#    y1 = nufft2(x, f, eps=eps)
+#    err = np.sqrt(np.sum(np.abs(y1 - y2) ** 2) / np.sum(np.abs(y1)**2))
+#    assert err < eps
+# 
+# 
+#def test_type_2():
+#    for eps in [1e-2, 1e-5, 1e-10, 1e-12]:
+#        _type_2(eps)
+# 
+# 
+#def _type_1_2_roundtrip(eps=1e-10):
+#    x, y1, f = _get_data_roundtrip()
+#    p = nufft1(x, y1, len(f), iflag=-1, eps=eps)
+#    y2 = len(x) / len(f) * nufft2(x, p, iflag=1, direct=True)
+#    err = np.sqrt(np.sum(np.abs(y1 - y2) ** 2) / np.sum(np.abs(y1)**2))
+#    assert err < eps
+# 
+# 
+#def test_type_1_2_roundtrip():
+#    for eps in [1e-2, 1e-5, 1e-10, 1e-12]:
+#        _type_1_2_roundtrip(eps)
+# 
+# 
+#def _type_3(eps=1e-10):
+#    x, y, f = _get_data()
+#    p2 = nufft3(x, y, f, direct=True)
+#    p1 = nufft3(x, y, f, eps=eps)
+#    err = np.sqrt(np.sum(np.abs(p1 - p2) ** 2) / np.sum(np.abs(p1)**2))
+#    assert err < eps
+# 
+# 
+#def test_type_3():
+#    for eps in [1e-2, 1e-5, 1e-10, 1e-12]:
+#        _type_3(eps)
+# 
+# 
+#def _type_1_and_3(eps=1e-10):
+#    x, y, f = _get_data()
+# 
+#    f = nufft1freqs(len(f))
+#    p2 = nufft3(x, y, f, eps=eps)
+#    p1 = nufft1(x, y, len(f), eps=eps)
+#    err = np.sqrt(np.sum(np.abs(p1 - p2) ** 2) / np.sum(np.abs(p1)**2))
+#    assert err < eps, "even"
+# 
+#    f = nufft1freqs(len(f) + 1)
+#    p2 = nufft3(x, y, f, eps=eps)
+#    p1 = nufft1(x, y, len(f), eps=eps)
+#    err = np.sqrt(np.sum(np.abs(p1 - p2) ** 2) / np.sum(np.abs(p1)**2))
+#    assert err < eps, "odd"
+# 
+#    df = 0.5 * (f[1] - f[0])
+#    p1 = nufft1(x, y, len(f), eps=eps, df=df)
+#    f = nufft1freqs(len(f), df=df)
+#    p2 = nufft3(x, y, f, eps=eps)
+#    err = np.sqrt(np.sum(np.abs(p1 - p2) ** 2) / np.sum(np.abs(p1)**2))
+#    assert err < eps, "even"
+# 
+# 
+#def test_1_and_3():
+#    for eps in [1e-2, 1e-5, 1e-10, 1e-12]:
+#        _type_1_and_3(eps)
